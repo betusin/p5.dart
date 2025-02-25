3e7aa0a12b4c66637554caf01956eb238408a2fb
Alžbeta Hajná <ahajna@typesoft.io>
2025-02-25 17:33:36 +0100

    map with constrain implemented

--- lib/painter.dart
+++ lib/painter.dart
@@ -422,4 +422,29 @@ class Painter extends ChangeNotifier implements CustomPainter {
   void mouseDragged() {}
 
   void mouseReleased() {}
+
+  // Math calculation
+
+  /// Constrains a number between a minimum and maximum value.
+  num constrain(num value, num low, num high) {
+    // TODO(betka): validate params
+    return value.clamp(low, high);
+  }
+
+  /// Maps a given `value` from one range (`inMin` to `inMax`) to another (`outMin` to `outMax`).
+  ///
+  /// This function performs a linear transformation of `value` so that a number in the
+  /// input range is proportionally scaled to the output range.
+  num pMap(num value, num start1, num stop1, num start2, num stop2, [bool withinBounds = false]) {
+    // TODO(betka): validate params
+    // return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+    final mappedValue = start2 + (value - start1) / (stop1 - start1) * (stop2 - start2);
+    if (!withinBounds) {
+      return mappedValue;
+    }
+    if (start2 < stop2) {
+      return constrain(mappedValue, start2, stop2);
+    }
+    return constrain(mappedValue, stop2, start2);
+  }
 }
